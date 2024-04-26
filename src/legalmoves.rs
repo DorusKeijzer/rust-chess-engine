6366422fc18f8fd45e496cf4e87f01b7fd7ef99b
Dorus Keijzer <83274869+DorusKeijzer@users.noreply.github.com>
2024-04-26 18:51:26 +0200

    made a start to horsey generation

--- src/legalmoves.rs
+++ src/legalmoves.rs
@@ -1,7 +1,24 @@
+use lazy_static::lazy_static;
+use std::sync::Mutex;
+use crate::utils;
 
-#[allow(dead_code)]
-fn PERFT(depth: i64)
+lazy_static! {
+    static ref KNIGHT_MOVES: [u64; 64] = init_knight_tables();
+}
+
+fn init_knight_tables() -> [u64;64]
 {
-    !todo!()
+    let mut res = [0;64];
+    for i in 0..63  
+    {
+        res[i] = utils::mask(i as u8);
+    }
+    res
+}
+
+
+fn perft()->()
+{
+    todo!();
 }
 
