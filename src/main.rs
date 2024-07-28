4b3201663b3d4e8af4d5607331c64892cfc0f09c
Dorus <doruskeijzer@gmail.com>
2024-07-28 10:46:56 +0200

    Added more perft tests

--- src/main.rs
+++ src/main.rs
@@ -951,6 +951,82 @@ mod tests {
             assert_eq!(perft(&mut board, 4, 4, false), 4_085_603);
             assert_eq!(perft(&mut board, 5, 5, false), 193_690_690);
         }
+        #[test]
+        fn perft_test_3() {
+            let mut board: Board = Board::new(Some("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - -  "));
+            let turn: Turn = Turn::White;
+            let mut state: State = State {
+                turn,
+                castling_rights: 0,
+                en_passant: None,
+            };
+            assert_eq!(perft(&mut board, 1, 1, false), 14);
+            assert_eq!(perft(&mut board, 2, 2, false), 191);
+            assert_eq!(perft(&mut board, 3, 3, false), 2812);
+            assert_eq!(perft(&mut board, 4, 4, false), 43_238);
+            assert_eq!(perft(&mut board, 5, 5, false), 746_624);
+        }
+        #[test]
+        fn perft_test_4() {
+            let mut board: Board = Board::new(Some(
+                "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1",
+            ));
+            let turn: Turn = Turn::White;
+            let mut state: State = State {
+                turn,
+                castling_rights: 0,
+                en_passant: None,
+            };
+
+            board.draw();
+            board.print_state();
+
+            println!("moves:");
+            let legalmoves = generate_legal_moves(&mut board);
+            for m in legalmoves {
+                println!("{}", m);
+            }
+
+            assert_eq!(perft(&mut board, 1, 1, false), 6);
+            assert_eq!(perft(&mut board, 2, 2, false), 264);
+            assert_eq!(perft(&mut board, 3, 3, false), 9467);
+            assert_eq!(perft(&mut board, 4, 4, false), 422_333);
+            assert_eq!(perft(&mut board, 5, 5, false), 15_833_292);
+        }
+        #[test]
+        fn perft_test_5() {
+            let mut board: Board = Board::new(Some(
+                " rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8  ",
+            ));
+            let turn: Turn = Turn::White;
+            let mut state: State = State {
+                turn,
+                castling_rights: 0,
+                en_passant: None,
+            };
+            assert_eq!(perft(&mut board, 1, 1, false), 44);
+            assert_eq!(perft(&mut board, 2, 2, false), 1486);
+            assert_eq!(perft(&mut board, 3, 3, false), 62_379);
+            assert_eq!(perft(&mut board, 4, 4, false), 2_103_487);
+            assert_eq!(perft(&mut board, 5, 5, false), 89_941_194);
+        }
+        #[test]
+        fn perft_test_6() {
+            let mut board: Board = Board::new(Some(
+                "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10",
+            ));
+            let turn: Turn = Turn::White;
+            let mut state: State = State {
+                turn,
+                castling_rights: 0,
+                en_passant: None,
+            };
+            assert_eq!(perft(&mut board, 1, 1, false), 1);
+            assert_eq!(perft(&mut board, 2, 2, false), 46);
+            assert_eq!(perft(&mut board, 3, 3, false), 2079);
+            assert_eq!(perft(&mut board, 4, 4, false), 89_890);
+            assert_eq!(perft(&mut board, 5, 5, false), 3_894_594);
+        }
         fn setup_board(fen: &str) -> (Board, State) {
             let board = Board::new(Some(fen));
             let state = State {
